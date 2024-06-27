8542de5534cf81ce17991a694aa89f00e20b2b56
panizolledotangel <angel.panizo@upm.es>
2024-06-27 19:09:25 +0200

    dummy project skeleton

---
+++ src/mylib.c
@@ -0,0 +1,14 @@
+#include "mylib.h"
+#include <stdio.h>
+
+void print_hello() {
+    printf("Hello, World!\n");
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
---
+++ test/test_mylib.c
@@ -0,0 +1,31 @@
+#include <unity.h>
+#include "mylib.h"
+
+void setUp(void) {
+    // Set up before each test
+}
+
+void tearDown(void) {
+    // Clean up after each test
+}
+
+void test_add(void) {
+    TEST_ASSERT_EQUAL(5, add(2, 3));
+    TEST_ASSERT_EQUAL(0, add(-1, 1));
+    TEST_ASSERT_EQUAL(-2, add(-1, -1));
+}
+
+void test_subtract(void) {
+    TEST_ASSERT_EQUAL(1, subtract(3, 2));
+    TEST_ASSERT_EQUAL(2, subtract(1, -1));
+    TEST_ASSERT_EQUAL(0, subtract(-1, -1));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_add);
+    RUN_TEST(test_subtract);
+
+    return UNITY_END();
+}

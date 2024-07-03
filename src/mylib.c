f650318dec509868beaa1fab3143c488a1665c6a
apanizolledot <angel.panizo@upm.es>
2024-07-03 17:05:54 +0200

    add not complient comment

--- src/mylib.c
+++ src/mylib.c
@@ -1,14 +1,17 @@
 #include "mylib.h"
 #include <stdio.h>
 
+/* Hello world */
 void print_hello() {
     printf("Hello, World!\n");
 }
 
+// suma
 int add(int a, int b) {
     return a + b;
 }
 
+// resta
 int subtract(int a, int b) {
     return a - b;
 }
